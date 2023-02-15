03cc6968cb4b47729cd5a184c73cbcea47111066
Dario <filatrella.dario@gmail.com>
2023-02-15 12:29:55 +0100

    merging dataframes

---
+++ Data/merger.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+btc_av = pd.read_csv("df_BTC_alphavantage_01_Feb.csv")
+eth_av = pd.read_csv("df_ETH_alphavantage_01_Feb.csv")
+reddit_c = pd.read_csv("reddit_comments.csv")
+reddit_p = pd.read_csv("reddit_posts.csv")
+twitter = pd.read_csv("tweeter_data_pandas.csv")
+
